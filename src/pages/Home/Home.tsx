8187c5d7fe27251457a0a1bea86dce870fab6b1d
sabovyan <abovyan.sargis90@gmail.com>
2020-11-06 11:25:22 +0400

    ci(home page)  add home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import SubmitButton from '../../components/FormElements/SubmitButton';
+import { Typography } from '@material-ui/core';
 
 function Home() {
+  const history = useHistory();
+  const handleRedirect: () => void = () => {
+    history.push('/registration/email');
+  };
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '80vh',
+      }}
+    >
+      <div
+        style={{
+          width: '500px',
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <img
+          style={{
+            width: '100%',
+          }}
+          src="flag.png"
+          alt="armenian flag"
+        />
+        <Typography
+          color="textSecondary"
+          variant="h5"
+          component="h2"
+          style={{
+            textAlign: 'center',
+            padding: '2rem 0 4rem',
+          }}
+        >
+          Support Armenia and Artsakh
+        </Typography>
+        <SubmitButton onClick={handleRedirect}>Register</SubmitButton>
+      </div>
     </div>
   );
 }
